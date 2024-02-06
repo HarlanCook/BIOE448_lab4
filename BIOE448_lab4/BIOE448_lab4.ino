df0f6234a786cee15247f558df64fd32c43c1d50
HarlanCook <81379974+HarlanCook@users.noreply.github.com>
2024-02-06 14:58:37 -0600

    Initial commit

---
+++ BIOE448_lab4/BIOE448_lab4.ino
@@ -0,0 +1,14 @@
+const int trigPin = A;
+const int echoPin = B;
+long duration;
+int distanceCm, distanceInch;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
